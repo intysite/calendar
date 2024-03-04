0db6b1c099ec817a890a0553a313903cd9f84d2d
Intisite <intysite@gmail.com>
2024-03-04 11:26:37 +0200

    Initial commit

---
+++ src/main/java/org/javaacademy/CalendarApp.java
@@ -0,0 +1,11 @@
+package org.javaacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarApp {
+    public static void main(String[] args) {
+        SpringApplication.run(CalendarApp.class);
+    }
+}

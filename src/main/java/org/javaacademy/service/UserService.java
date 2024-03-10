f74dc4802e553494fc047b48451d08554556ef53
Intisite <intysite@gmail.com>
2024-03-10 12:51:47 +0200

    Created service

---
+++ src/main/java/org/javaacademy/service/UserService.java
@@ -0,0 +1,10 @@
+package org.javaacademy.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class UserService {
+}

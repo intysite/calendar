539b614f0cf9ad28e222cb10eb3276fc40b1418c
Intisite <intysite@gmail.com>
2024-03-19 22:20:59 +0200

    Development event controller

--- src/main/java/org/javaacademy/controller/EventController.java
+++ src/main/java/org/javaacademy/controller/EventController.java
@@ -1,11 +1,15 @@
 package org.javaacademy.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.javaacademy.dto.EventDto;
 import org.javaacademy.service.EventService;
 import org.javaacademy.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,4 +18,12 @@ public class EventController {
     private final EventService eventService;
     private final UserService userService;
 
+    @PostMapping("/event/{token")
+    public ResponseEntity<?> createEvent(@PathVariable String token, @RequestBody EventDto eventDto) {
+        if (token == null) {
+            return new ResponseEntity<>("Токен пользователя не задан", HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(eventService.createEvent(userService.getUserByToken(token), eventDto),
+                                    HttpStatus.OK);
+    }
 }

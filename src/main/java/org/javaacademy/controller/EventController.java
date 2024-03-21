b8a0948fe31c6358b19e1071b1a6b37a6fb55909
Intisite <intysite@gmail.com>
2024-03-21 21:00:38 +0200

    Development event controller

--- src/main/java/org/javaacademy/controller/EventController.java
+++ src/main/java/org/javaacademy/controller/EventController.java
@@ -7,10 +7,7 @@ import org.javaacademy.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -25,5 +22,10 @@ public class EventController {
         }
         return new ResponseEntity<>(eventService.createEvent(userService.getUserByToken(token), eventDto),
                                     HttpStatus.OK);
+    }
+
+    @GetMapping("/event/{token}")
+    public ResponseEntity<?> getEventsByUser(@PathVariable String token) {
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
